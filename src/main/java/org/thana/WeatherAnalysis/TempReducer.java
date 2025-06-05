dc4968a6676bd0e3191aec1105175a043ccddce0
Thavamohan Thananchayan <thananchayan26@gmail.com>
2025-06-05 15:59:45 +0530

    reducer function modified

--- src/main/java/org/thana/WeatherAnalysis/TempReducer.java
+++ src/main/java/org/thana/WeatherAnalysis/TempReducer.java
@@ -5,16 +5,30 @@ import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class TempReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
-  public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+public class TempReducer extends Reducer<Text, DoubleWritable, Text, Text> {
+
+  @Override
+  protected void setup(Context context) throws IOException, InterruptedException {
+    context.write(new Text(String.format("%-30s %s", "Country", "Average Temperature")), null);
+    context.write(new Text("----------------------------------------------------------"), null);
+  }
+
+  @Override
+  public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+      throws IOException, InterruptedException {
+
     double sum = 0;
     int count = 0;
+
     for (DoubleWritable val : values) {
       sum += val.get();
       count++;
     }
+
     if (count > 0) {
-      context.write(key, new DoubleWritable(sum / count));
+      double average = sum / count;
+      String formattedLine = String.format("%-30s %8.2f", key.toString(), average);
+      context.write(new Text(formattedLine), null);
     }
   }
 }
